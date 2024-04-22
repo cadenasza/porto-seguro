221d9534fe79e20df97fa9cfa6785ed32bd81e2b
cadenasza <leonardocadena2005@gmail.com>
2024-04-22 17:48:45 -0300

    Adicionando comentários no código e cirando um loop while true para o caastro de veículos

--- porto.py
+++ porto.py
@@ -2,14 +2,17 @@ print('-'* 31)
 print('    Centro Automotivo Porto')
 print('-'* 31)
 
+#criação de uma lista com os serviços disponiveis para o cliente
 servicos = ['Alinhamento', 'Ar-condicionado', 'Arrefecimento', 
             'Balanceamento e geometria', 'Correias', 'Discos e Patilhas de Freio', 
             'Embreagens', 'Filtros e Velas de Ignição']
 
+#criação de um dicionário para os orçamentos de cada serviço disponivel
 orcamentos = {'alinhamento': 1300, 'Ar-condicionado': 300, 'Arrefecimento': 400, 
             'Balanceamento e geometria': 200, 'Correias': 300, 'Discos e Patilhas de Freio': 500,
             'Embreagens': 250, 'Filtros e Velas de Ignição': 375}
 
+#lista criada para os veiculos que precisam serão adicionados
 veiculos = []
 
 while True:
@@ -29,27 +32,36 @@ while True:
             print(f'- {servico}')
         print()   
 
+    # Na escolha 2, o programa irá mostrar o serviço e seu orçamento (o para serviço e s para orçamento)
     elif escolha == 2:
-        for o, p in orcamentos.items():
+        for s, o in orcamentos.items():
             print()
-            print(f'- {o}: R${p}')
+            print(f'- {s}: R${o}')
         print()
 
+    # Na escolha 3, irá fazer um laço infinito para que o usuário possa cadastrar quantos veículos quiser, finalizará quando o usuarío digitar Nao
     elif escolha == 3:
-        print('-' * 25)
-        nome_veiculo = input(' Nome do veiculo: ')
-        print('-' * 25)
-        veiculos.append(nome_veiculo)
+        while True:
+            print('-' * 25)
+            nome_veiculo = input(' Nome do veiculo: ')
+            print('-' * 25)
+            veiculos.append(nome_veiculo)
+            continuar = input('Deseja continuar? [Sim/Nao]').capitalize() # .capitalize() deixa a primeira letra maiuscula
+            if continuar == 'Nao':
+                break
 
+    # Na escolha 4, a quantidade de veiculos cadastrados na lista veiculos será mostrada
     elif escolha == 4:
          print('-' * 30)
          print(f'Estão cadastrados {len(veiculos)} veiculo(s)')
          print('-' * 30)
 
+    # Na escolha 5, o programa encerra
     elif escolha == 5:
         print('Finalizando o programa...')
         break
     
+    # Ao digitar qualquer outra opção que não está no menu irá mostrar uma mensagem de opção invalida
     else:
         print('Opcao invalida! Digite novamente')
         print()
