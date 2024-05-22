46d9ca3ca7a11ee6477f2a07d29ac6753a0ee0c2
cadenasza <leonardocadena2005@gmail.com>
2024-05-22 09:51:12 -0300

    atualizando o codigo para a sprint 2

--- porto.py
+++ porto.py
@@ -1,7 +1,3 @@
-print('-'* 31)
-print('    Centro Automotivo Porto')
-print('-'* 31)
-
 #criação de uma lista com os serviços disponiveis para o cliente
 servicos = ['Alinhamento', 'Ar-condicionado', 'Arrefecimento', 
             'Balanceamento e geometria', 'Correias', 'Discos e Patilhas de Freio', 
@@ -10,47 +6,91 @@ servicos = ['Alinhamento', 'Ar-condicionado', 'Arrefecimento',
 #lista criada para os veiculos que precisam serão adicionados
 veiculos = []
 
-while True:
+placas = []
+
+clientes = {}
+# função criada para o menu do programa
+def menu():
+    print('-'* 40)
+    print('    Centro Automotivo Porto\n')
     print(''' Menu de opções 
     [1] - Digite 1 para vizualizar o serviços disponiveis
     [2] - digite 2 para cadastrar o veículo
-    [3] - digite 3 para ver quantos veiculos estão cadastrados
-    [4] - digite 4 para finalizar o programa''')
-    
+    [3] - digite 3 para ver quais veiculos estão cadastrados
+    [4] - digite 4 para se cadastrar
+    [5] - digite 5 para finalizar o programa''')
+    print('-'* 40)
 
-    escolha = int(input('digite sua escolha: '))
 
-    # Na escolha 1, irá ser mostrados os serviços um a um
-    if escolha == 1:
-        for servico in servicos:
-            print()
-            print(f'- {servico}')
-        print()   
-
-     # Na escolha 2, irá fazer um laço infinito para que o usuário possa cadastrar os veículos que precisam de um diagnóstico, finalizará quando o usuarío digitar Nao
-    elif escolha == 2:
-        while True:
-            print('-' * 25)
-            nome_veiculo = input(' Nome do veiculo: ')
-            print('-' * 25)
-            veiculos.append(nome_veiculo)
-            continuar = input('Deseja cadastrar outro? [Sim/Nao]').capitalize() # .capitalize() deixa a primeira letra maiuscula
-            if continuar == 'Nao':
-                break
-
-    # Na escolha 3, a quantidade de veiculos cadastrados na lista veiculos será mostrada
-    elif escolha == 3:
-        print('-' * 30)
-        print(f'Estão cadastrados {len(veiculos)} veiculo(s)')
-        print('-' * 30)
-    
-     # Na escolha 4, o programa encerra
-    elif escolha == 4:
-        print('Finalizando o programa...')
-        break
-
-    # Ao digitar qualquer outra opção que não está no menu irá mostrar uma mensagem de opção invalida
-    else:
-        print('Opcao invalida! Digite novamente')
+# função criada para mostrar os servicos disponiveis
+def servicos_disponiveis(servicos):
+    for servico in servicos:
         print()
+        print(f'- {servico}')
+             
+
+
+#função criada para o cadastro de veiculos, nas listas veiculos e placas
+def cadastro_veiculo():
+    while True:
+        print('-' * 25)
+        nome_veiculo = input(' Nome do veiculo: ')
+        placa_veiculo = input('Placa do veiculo: ')
+        veiculos.append(nome_veiculo)
+        placas.append(placa_veiculo)
+        print('-' * 25)
+        continuar = input('Deseja cadastrar outro? [Sim/Nao]').capitalize() # .capitalize() deixa a primeira letra maiuscula
+        if continuar == 'Nao':
+            break
+
+
+#função criada para ver o nome de cada carro cadastrado
+def veiculos_cadastrados(veiculos, placas):
+    print('-' * 30)
+    for pos, veiculo in enumerate(veiculos):
+        print(f'{pos + 1}- Veiculo: {veiculo}')
+    for posicao, placa in enumerate(placas):
+        print(f'{posicao + 1}- Placa: {placa}')  
+
+
+def cadastro_cliente(clientes):
+    print('Cadastro')
+    print('-'*30)
+    clientes['nome'] = input('Nome: ')
+    clientes['email'] = input('Email: ')
+    print('Cadastro realizado!')
+    print('-'*30)
+    
+
+            
+def main():
+    while True:
+        menu()
+        escolha = int(input('digite sua escolha: '))
+
+        # Na escolha 1, irá ser mostrados os serviços um a um
+        if escolha == 1:
+            print(servicos_disponiveis(servicos))
+
+        # Na escolha 2, irá fazer um laço infinito para que o usuário possa cadastrar os veículos que precisam de um diagnóstico, finalizará quando o usuarío digitar Nao
+        elif escolha == 2:
+            cadastro_veiculo()
+
+        # Na escolha 3, a usuario ira ver os veiculos e placas cadastrados
+        elif escolha == 3:
+            veiculos_cadastrados(veiculos, placas)
+
+        elif escolha == 4:
+            cadastro_cliente(clientes)
+
+        # Na escolha 4, o programa encerra
+        elif escolha == 5:
+            print('Finalizando o programa...')
+            break
+
+        # Ao digitar qualquer outra opção que não está no menu irá mostrar uma mensagem de opção invalida
+        else:
+            print('Opcao invalida! Digite novamente')
+            print()
  
+main()
